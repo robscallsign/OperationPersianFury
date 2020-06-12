91e2b689bcc97f33c5a5ad77aa59bc5dd6a985c4
Rob <49131756+robscallsign@users.noreply.github.com>
2020-06-12 12:38:53 -0700

    adding recon script demo mission

---
+++ demo_missions/recon_demo_loader.lua
@@ -0,0 +1,10 @@
+-- To use this loader create a Trigger in the mission
+-- Time more (1), DO SCRIPT:
+-- assert(loadfile("C:/HypeMan/ps3_tech/demo_missions/recon_demo_loader.lua"))()
+
+assert(loadfile("C:/HypeMan/mist.lua"))()
+assert(loadfile("C:/HypeMan/Moose.lua"))()
+JSON = (loadfile "C:/HypeMan/skynet/JSON.lua")() -- one-time load of the routines
+assert(loadfile("C:/HypeMan/HypeMan.lua"))()
+
+assert(loadfile("C:/HypeMan/ps3_tech/recon_script.lua"))()
